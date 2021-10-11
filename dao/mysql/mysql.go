7807d1fe4161ae5e0b6f97b0537a09d6e55f9d06
moretai <moretaiupupup@gamil.com>
2021-10-11 23:41:56 +0800

    init & left mysql init

---
+++ dao/mysql/mysql.go
@@ -0,0 +1,38 @@
+package mysql
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"github.com/spf13/viper"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func Init() (err error) {
+	// DSN:Data Source Name
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		viper.GetString("mysql.user"),
+		viper.GetString("mysql.password"),
+		viper.GetString("mysql.host"),
+		viper.GetInt("mysql.port"),
+		viper.GetString("mysql.dbname"),
+	)
+
+	if db, err = sqlx.Connect("mysql", dsn); err != nil {
+		zap.L().Error("init mysql failed", zap.Error(err))
+		return
+	}
+
+	db.SetMaxOpenConns(viper.GetInt("mysql.max_open_conns"))
+	db.SetMaxIdleConns(viper.GetInt("mysql.max_idle_conns"))
+	return
+}
+
+func Close() {
+	_ = db.Close()
+}
